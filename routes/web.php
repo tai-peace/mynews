61777bcfc1daa3497d2b8023cfa0c0b7f8775d8d
”名前” <sokakaka1125@icloud.com>
2019-10-16 13:08:25 +0000

    課題09

--- routes/web.php
+++ routes/web.php
@@ -15,5 +15,6 @@ Route::get('/', function () {
     return view('welcome');
 });
 Route::group(['prefix' => 'admin'], function() {
-    Route::get('news/create', 'Admin\NewsController@add');
+    Route::get('profile/create', 'Admin\ProfileController@add');
 });
+Route::get('admin/profile/edit', 'Admin\ProfileController@add');
