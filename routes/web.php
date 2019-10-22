2a60e3168afd6225c794683cc78fa64e1323da89
”名前” <sokakaka1125@icloud.com>
2019-10-22 03:35:37 +0000

    課題12

--- routes/web.php
+++ routes/web.php
@@ -14,9 +14,15 @@
 Route::get('/', function () {
     return view('welcome');
 });
-Route::group(['prefix' => 'admin'], function() {
-    Route::get('news/create', 'Admin\NewsController@add');
-    Route::get('profile/edit', 'Admin\ProfileController@add');
-    Route::get('profile/create', 'Admin\ProfileController@add');
+Route::group(['prefix' => 'admin','middleware' => 'auth'], function() {
+    Route::get('news/create', 'Admin\NewsController@add')->middleware('auth');
+    Route::post('news/create', 'Admin\NewsController@create'); 
+    Route::get('profile/edit', 'Admin\ProfileController@add')->middleware('auth');
+    Route::get('profile/create', 'Admin\ProfileController@add')->middleware('auth');
+    Route::get('profile/edit', 'Admin\ProfileController@edit')->middleware('auth');
 });
 
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
